40b817839f9987fe1c2a61b4090b145034381e02
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-15 16:31:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,3 +53,24 @@ def reduce_to_total( source_array , starting_point )
   total
 end
 
+
+def reduce_to_all_true( source_array , starting_point )
+  total = 0 
+  i = 0 
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_any_true( source_array , starting_point )
+  total = 0 
+  i = 0 
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
