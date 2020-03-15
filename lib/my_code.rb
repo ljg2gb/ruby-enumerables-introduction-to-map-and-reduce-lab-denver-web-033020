f1485f469e2247f22a5d9f9f090f54b79a7590da
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-15 16:27:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,9 @@ def reduce_to_total(source_array, starting_point)
   total = 0 
   i = 0 
   while i < source_array.length do
-    reduced_value
-  
+    total += source_array[i]
+    i += 1
+  end
+  total
 end
 
