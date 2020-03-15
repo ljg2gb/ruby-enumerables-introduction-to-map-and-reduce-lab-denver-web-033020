c0940054a865d331e2a4b35056bdd07ee4ccf8e9
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-15 16:21:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # My Code here....
+
+def map_to_negativize(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array << source_array[i] * -1
+  i += 1
+  end 
+  new_array
+end
